cdcaaa6f90dec4690d465ac1bdf2f5d66fc04bdb
angelynafaa <angelyna826@gmail.com>
2020-08-21 08:00:00 +0700

    new route

---
+++ src/views/Product.vue
@@ -0,0 +1,21 @@
+<templates>
+    <div>
+        <section class="wrapper">
+            <h2> {{product.name}}</h2>
+        </section>
+    </div>
+</templates>
+
+<script>
+export default {
+  name: "product",
+  data() {
+    return {
+      product: this.$store.getters.product(this.$route.paramas.id)
+    }
+}
+};
+</script>
+
+<style lang="scss">
+</style>
