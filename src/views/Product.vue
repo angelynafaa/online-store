b2a42a5a867720d92ad2c73095d278c66dbbf490
angelynafaa <angelyna826@gmail.com>
2020-08-22 22:41:49 +0700

    add button to add cart

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -2,19 +2,24 @@
   <div>
     <section class="wrapper">
       <div class="flex-col">
-        <img  class="flex-col--2" :src="makeImagePath(product)" alt="">
-        <div class="flex-col--2" >
+        <img class="flex-col--2" :src="makeImagePath(product)" alt="" />
+        <div class="flex-col--2">
           <h2>{{ product.name }}</h2>
+          <button @click="addToCart" class="btn btn--grey">Add to cart</button>
           <p>Price: ${{ product.price }}</p>
           <p>Size: {{ product.size }}</p>
           <p>Color: {{ product.color }}</p>
-          <p><em>{{ product.quantity }} left in stock</em></p>
+          <p>
+            <em>{{ product.quantity }} left in stock</em>
+          </p>
           <h3>Details</h3>
           <ul>
             <li>Material: {{ product.details.material }}</li>
             <li>Fit: {{ product.details.fit }}</li>
             <li>Maintenance: {{ product.details.maintenance }}</li>
-            <li v-if="product.details.additional">Additional: {{ product.details.additional }}</li>
+            <li v-if="product.details.additional">
+              Additional: {{ product.details.additional }}
+            </li>
           </ul>
         </div>
       </div>
@@ -23,14 +28,14 @@
 </template>
 
 <script>
-import { imagePath } from '@/mixins/imagePath.js'
+import { imagePath } from "@/mixins/imagePath.js";
 export default {
-  name: 'product',
+  name: "product",
   mixins: [imagePath],
   data() {
     return {
       product: this.$store.getters.product(this.$route.params.id)
-    }
+    };
   }
 };
 </script>
@@ -42,6 +47,6 @@ export default {
 }
 .flex-col--2 {
   width: 50%;
-  text-align:left;
+  text-align: left;
 }
 </style>
