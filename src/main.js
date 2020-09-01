a9393d5b10d10e7483fa1c3700c0d54dc359c0ce
angelynafaa <angelyna826@gmail.com>
2020-09-01 23:12:13 +0700

    add

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ import router from "./router";
 import store from "./store";
 import BootstrapVue from "bootstrap-vue";
 import IconsPlugin from "bootstrap-vue";
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.config.productionTip = false;
