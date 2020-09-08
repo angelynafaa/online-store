d18141ec3b02f6f020945e70c296b15970e762da
angelynafaa <angelyna826@gmail.com>
2020-09-08 23:18:21 +0700

    benerein tampilan home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,52 @@
 <template>
-  <body>
-    <header>
-      <div
-        style="padding-bottom: 0px;
-    border-top-width: 54px;
-    padding-top: 187px"
-      >
-        <div class="ima">
-          <h1 class="new">NEW COLLECTION</h1>
-          <br />
-          <h1 class="brand">SS60</h1>
+  <b-container class="home-row">
+    <b-row>
+      <b-col cols="5" fluid>
+        <h1 class=" brand">My Fashion</h1>
+        <h2 class="merk">SS60</h2>
+      </b-col>
+      <b-col cols="7">
+        <div class="clearfix">
           <div>
-            <b-button-group>
-              <b-button>MEN</b-button>
-              <b-button>WOMEN</b-button>
-            </b-button-group>
+            <div class="box">
+              <b-img
+                class="img-brand"
+                src="../assets/bg-1.png"
+                fluid
+                alt="Responsive image"
+              ></b-img>
+            </div>
           </div>
         </div>
-        <b-card
-          img-src="../assets/bg-1.png"
-          img-alt="Image"
-          style="max-width: 37rem;"
-          class="mb-2"
-        >
-        </b-card>
-      </div>
+      </b-col>
+    </b-row>
+  </b-container>
 
-      <div class="container box" style=""></div>
-    </header>
-    <!-- <section class="wrapper">
+  <!-- <div
+      style="padding-bottom: 0px;
+    border-top-width: 54px;
+    padding-top: 187px"
+    >
+      <div class="ima">
+        <h1 class="new">NEW COLLECTION</h1>
+        <br />
+        <div>
+          <b-button-group>
+            <b-button>MEN</b-button>
+            <b-button>WOMEN</b-button>
+          </b-button-group>
+        </div>
+      </div>
+      <b-card
+        img-src="../assets/bg-1.png"
+        img-alt="Image"
+        style="max-width: 37rem;"
+        class="mb-2"
+      >
+      </b-card>
+    </div> -->
+
+  <!-- <section class="wrapper">
       <h2>Featured Items</h2>
       <ul class="featured-items">
         <li
@@ -46,12 +64,10 @@
         </li>
       </ul>
     </section> -->
-  </body>
 </template>
 
 <script>
 import { imagePath } from "@/mixins/imagePath.js";
-
 export default {
   name: "home",
   mixins: [imagePath],
@@ -68,116 +84,125 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.wrapper {
-  max-width: 800px;
-  margin: 0 auto;
-  @media only screen and (max-width: 832px) {
-    max-width: 100%;
-    padding: 1rem;
-    text-align: center;
-  }
-}
-.featured-items {
-  padding-left: 0;
-  list-style: none;
-  display: flex;
-  justify-content: space-between;
-  @media only screen and (max-width: 832px) {
-    flex-direction: column;
-  }
-}
-.featured-items__item {
-  width: 33%;
-  text-align: center;
-  @media only screen and (max-width: 832px) {
-    width: 100%;
-  }
-}
-.product-image {
-  max-height: 200px;
-}
-.product-title {
-  font-weight: bold;
-}
-
-header {
-  padding: 100px 0 165px;
-  margin-top: -72px;
-  background-color: #f1e6e6;
-  // background-image: url(../assets/bg-1.png);
-  background-size: cover;
+//
+<style lang="sass" scoped>
 
   h1,
-  p {
-    color: darkblue;
-  }
-  h1 {
-    font-family: "playfair Display", serif;
-    font-weight: bold;
-    font-size: 35px;
-  }
-  p {
-    font-size: 16px;
-  }
+    .brand
 
-  .btn-get-started {
-    background-color: #ff9e53;
-    color: white;
+       color: white
+       font-size: 3em
+       text-align: end
+       margin-top: 167px
+  h2,
+    .merk
+      color: black
+      margin-left: 110px
+      font-size: 7em
+      
 
-    &:hover {
-      background-color: #ff9e69;
-      color: white;
-    }
-  }
-}
+  home-row
+    padding: 20px !important
 
-header {
-  .card {
-    background-color: #da53626e !important;
-    margin-left: 50%;
-    margin-bottom: 100px;
-    margin-top: -555px;
-    position: static;
-  }
-}
+ 
 
-.card-img,
-.card-img-top {
-  margin-top: -50px !important;
-}
+  .clearfix
+  .box
+    height: 309px
+    background-color: #e24c6a8f
+    position:relative
+    margin-top: 243px
+    .img-brand
+      margin-top: -204px !important
 
-.card-body {
-  padding: 0 !important;
-}
+  img
+    .img-brand
+      padding-top: 20px
+// .wrapper
+//   max-width: 800px
+//   margin: 0 auto
+//   @media only screen and (max-width: 1066px)
+//     max-width: 100%
+//     padding: 1rem
+//     text-align: center
 
-.ima {
-  min-height: 540px;
-  background-color: #f1e6e6;
-  margin-right: 50%;
-  margin-top: -62px;
-  h1,
-  .new {
-    color: #000000d4;
-    text-align: right;
-    margin-right: 81px;
-  }
-  .brand {
-    color: black;
-    font-size: 297px;
-    margin-right: -75px;
-  }
-}
+// .featured-items
+//   padding-left: 0
+//   list-style: none
+//   display: flex
+//   justify-content: space-between
+//   @media only screen and (max-width: 1066px)
+//     flex-direction: column
 
-div {
-  .btn-group {
-    margin-left: 65% !important;
-  }
-  .btn-secondary {
-    border-bottom-right-radius: 22px;
-    border-top-right-radius: 22px;
-    border-bottom-left-radius: 22px;
-    border-top-left-radius: 22px;
-  }
-}
+// .featured-items__item
+//   width: 33%
+//   text-align: center
+//   @media only screen and (max-width: 1066px)
+//     width: 100%
+
+// .product-image
+//   max-height: 200px
+// .product-title
+//   font-weight: bold
+
+
+// header
+//   padding: 100px 0 165px
+//   margin-top: -72px
+//   background-color: #f1e6e6
+//   // background-image: url(../assets/bg-1.png);
+//   background-size: cover
+
+//   h1,
+//   p
+//     color: darkblue
+
+//   h1
+//     font-family: "playfair Display", serif
+//     font-weight: bold
+//     font-size: 35px
+
+//   p
+//     font-size: 16px
+// header
+//   .card
+//     background-color: #da53626e !important
+//     margin-left: 50%
+//     margin-bottom: 100px
+//     margin-top: -555px
+//     position: static
+
+// .card-img,
+// .card-img-top
+//   margin-top: -50px !important
+
+// .card-body
+//   padding: 0 !important
+
+// @media only screen and (min-width: 600px)
+// .ima
+//   min-height: 540px
+//   background-color: #f1e6e6
+//   margin-right: 50%
+//   margin-top: -62px
+//   h1,
+//   .new
+//     color: #000000d4
+//     text-align: right
+//     margin-right: 81px
+
+//   .brand
+//     color: black
+//     font-size: 297px
+//     margin-right: -75px
+
+// div
+//   .btn-group
+//     margin-left: 65% !important
+//   .btn-secondary
+//     border-bottom-right-radius: 22px
+//     border-top-right-radius: 22px
+//     border-bottom-left-radius: 22px
+//     border-top-left-radius: 22px
+//
 </style>
