2df4f6cadf3eaffd3bf044b9c4300b0d5b3b6985
angelynafaa <angelyna826@gmail.com>
2020-09-12 23:07:17 +0700

    edit product page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,10 +1,48 @@
 <template>
   <div>
-    <section class="wrapper">
+    <h1 style="padding-top:9%; text-align:center;">{{ product.name }}</h1>
+    <b-row style="padding-top: 5%;">
+      <b-col cols="6">
+        <b-row>
+          <b-col>
+            <div
+              class="box"
+              style="background-color: #0000002b;
+                      border-radius: 30px;"
+            >
+              <b-img
+                fluid-grow
+                :src="makeImagePath(product)"
+                alt="Image 1"
+                style="position: relative;
+                    right: 20px;
+                    border-radius: 25px;
+                    "
+              >
+              </b-img>
+            </div>
+          </b-col>
+        </b-row>
+      </b-col>
+      <b-col cols="6">
+        <h1 style="text-align:center; font-size:4rem; color:white">
+          ${{ product.price }}
+        </h1>
+        <!-- <div class="detail" style="font-size:1.5rem;"> -->
+          <h3 v-if="product.details.additional">
+            Additional: {{ product.details.additional }}<br />
+            Material: {{ product.details.material }} <br />
+            Maintenance: {{ product.details.maintenance }} <br />
+            Fit: {{ product.details.fit }}
+          </h3>
+        <!-- </div> -->
+      </b-col>
+    </b-row>
+
+    <!-- <section class="wrapper">
       <div class="flex-col">
         <img class="flex-col--2" :src="makeImagePath(product)" alt="" />
         <div class="flex-col--2">
-          <h2>{{ product.name }}</h2>
           <button @click="addToCart" class="btn btn--grey">Add to Cart</button>
           <p>Price: ${{ product.price }}</p>
           <p>Size: {{ product.size }}</p>
@@ -23,7 +61,7 @@
           </ul>
         </div>
       </div>
-    </section>
+    </section> -->
   </div>
 </template>
 
@@ -45,37 +83,41 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.flex-col {
-  display: flex;
-  align-items: flex-start;
-}
-.flex-col--2 {
-  width: 50%;
-}
-.flex-justify-right {
-  text-align: right;
-}
-.flex-col--align-center {
-  align-items: center;
-}
-.btn {
-  padding: 0.5rem 0.75rem;
-  border-radius: 3px;
-  border: none;
-  background-color: transparent;
-  font-size: 0.9rem;
-  font-weight: bold;
-  cursor: pointer;
-  transition: all 0.15s ease;
-}
+<style lang="sass" scoped>
+.flex-col
+  display: flex
+  align-items: flex-start
 
-.btn--grey {
-  background-color: #2c3e50;
-  color: #fff;
+
+
+.flex-col--2
+  width: 50%
+
+
+.flex-justify-right
+  text-align: right
+
+
+
+.flex-col--align-center
+  align-items: center
+
+
+.btn
+  padding: 0.5rem 0.75rem
+  border-radius: 3px
+  border: none
+  background-color: transparent
+  font-size: 0.9rem
+  font-weight: bold
+  cursor: pointer
+  transition: all 0.15s ease
+
+
+.btn--grey
+  background-color: #2c3e50
+  color: #fff
   &:hover,
-  &:focus {
-    background-color: #42b983;
-  }
-}
+  &:focus
+    background-color: #42b983
 </style>
